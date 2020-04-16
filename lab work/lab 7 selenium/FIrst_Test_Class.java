1515b91508359bca0b1701db150f8e60571b74de
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-16 20:01:20 +0530

    Update FIrst_Test_Class.java

--- lab work/lab 7 selenium/FIrst_Test_Class.java
+++ lab work/lab 7 selenium/FIrst_Test_Class.java
@@ -1,5 +1,5 @@
 package demo_selenium;
-
+//webdriver is class implemented by chromedriver
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,11 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FIrst_Test_Class {
 
 	public static void main(String[] args) {
-	System.setProperty("webdriver.chrome.driver","/Users/piyushvishnoi/Desktop/chromedriver/chromedriver");
+	System.setProperty("webdriver.chrome.driver","/Users/piyushvishnoi/Desktop/chromedriver/chromedriver"); //setting property
 WebDriver driver=new ChromeDriver();
-driver.navigate().to("https://www.google.co.in");
-driver.findElement(By.name("q")).sendKeys("Devops");
-driver.findElement(By.name("btnK")).click();
+driver.navigate().to("https://www.google.co.in");  //how to navigate to a site
+driver.findElement(By.name("q")).sendKeys("Devops");   //to type devops at search area on google
+driver.findElement(By.name("btnK")).click();  //to click search button on google
 	}
 
 }
