6ce4f6233956eaa39c3036ceaecf0b6199066ebd
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-16 19:33:01 +0530

    First test class using selenium

---
+++ lab work/lab 7 selenium/FIrst_Test_Class.java
@@ -0,0 +1,17 @@
+package demo_selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FIrst_Test_Class {
+
+	public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver","/Users/piyushvishnoi/Desktop/chromedriver/chromedriver");
+WebDriver driver=new ChromeDriver();
+driver.navigate().to("https://www.google.co.in");
+driver.findElement(By.name("q")).sendKeys("Devops");
+driver.findElement(By.name("btnK")).click();
+	}
+
+}
