51f3e196faa88cedbd63171ce0ffe4b26d8c3a60
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-16 20:59:56 +0530

    Update web_command.java

--- lab work/lab 7 selenium/web_command.java
+++ lab work/lab 7 selenium/web_command.java
@@ -9,21 +9,21 @@ public class web_command {
 		System.setProperty("webdriver.chrome.driver","/Users/piyushvishnoi/Desktop/chromedriver/chromedriver");
 		WebDriver driver=new ChromeDriver();
 		String url=("https://www.google.co.in/");
-		driver.get(url);
+		driver.get(url); // to get url name `
 		String title=driver.getTitle();
-		int titlelength=driver.getTitle().length();
+		int titlelength=driver.getTitle().length();  //to get title length
 		System.out.println("title of page is: " +title);
 		System.out.println("title length is: " +titlelength);
-String actualurl=driver.getCurrentUrl();
-if(actualurl.equals("https://www.google.co.in/"))
+String actualurl=driver.getCurrentUrl(); //to get url name upon which we are working upon
+if(actualurl.equals("https://www.google.co.in/"))  //comparing our url and loaded url
 {
 	System.out.println("Correct url got opened");
 }
 else {
 	System.out.println("Correct url not opened");
 }
-String pagesource=driver.getPageSource();
-int pagesourcelength=pagesource.length();
+String pagesource=driver.getPageSource();  //to get pagesource upon which we are working upon
+int pagesourcelength=pagesource.length();  //to get length of the pagesource
 System.out.println("page source content conatins "+pagesourcelength+" no of words");
 driver.close();
 	}
