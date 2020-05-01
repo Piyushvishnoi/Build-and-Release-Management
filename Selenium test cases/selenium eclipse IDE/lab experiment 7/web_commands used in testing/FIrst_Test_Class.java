d43e05967f502406f4c54255c5bfcde3bbd58d00
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-05-02 05:17:29 +0530

    Update FIrst_Test_Class.java

--- Selenium test cases/selenium eclipse IDE/lab experiment 7/web_commands used in testing/FIrst_Test_Class.java
+++ Selenium test cases/selenium eclipse IDE/lab experiment 7/web_commands used in testing/FIrst_Test_Class.java
@@ -1,7 +1,7 @@
 package demo_selenium;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver; 
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FIrst_Test_Class {
